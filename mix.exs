870f134c6a29e28f526ff38d45f3f6b58376f63e
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-13 19:39:38 +0200

    Add dependencies: - tesla - finch - jason

--- mix.exs
+++ mix.exs
@@ -24,6 +24,9 @@ defmodule DeeplEx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:tesla, "~>1.4"},
+      {:finch, "~>0.13"},
+      {:jason, "~>1.4"}
     ]
   end
 end
